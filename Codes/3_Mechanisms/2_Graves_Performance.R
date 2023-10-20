8817c8f7084c9b29fe44692c362ee58efc89cd8a
riccardo-df <difrancesco.riccardo96@gmail.com>
2023-10-20 13:51:22 +0200

    Update 2_Graves_Performance.R

--- Codes/3_Mechanisms/2_Graves_Performance.R
+++ Codes/3_Mechanisms/2_Graves_Performance.R
@@ -19,6 +19,7 @@ inst <- lapply(pkgs, library, character.only = TRUE)
 champions <- "Graves"
 
 ## Select outcome series.
+outcome_colnames <- "win_rate_pooled"
 bandwidth <- 3
 
 min_date <- as.POSIXct("2022-01-01", tryFormats = "%Y-%m-%d")
@@ -36,65 +37,17 @@ covariates <- c()
 # covariates <- c("ban_rate_pooled", "win_rate_pooled", "gold_pooled", "kills_pooled", "assists_pooled", "deaths_pooled")
 
 # Estimation --------------------------------------------------------------
-pooled_result_kd_ratio <- run_main_pooled(champions, "kd_ratio", donors, estimator, treatment_date, backdate, inference = inference, n_boot = n_boot, bandwidth = bandwidth, covariate_colnames = covariates, max_date = max_date)
-pooled_result_win_rate <- run_main_pooled(champions, "win_rate_pooled", donors, estimator, treatment_date, backdate, inference = inference, n_boot = n_boot, bandwidth = bandwidth, covariate_colnames = covariates, max_date = max_date)
+pooled_result <- run_main_pooled(champions, outcome_colnames, donors, estimator, treatment_date, backdate, inference = inference, n_boot = n_boot, bandwidth = bandwidth, covariate_colnames = covariates, max_date = max_date)
 
 # Plots -------------------------------------------------------------------
 save_here <- "C:/Users/difra/Dropbox/University/Research/LoL/2_Data_Collection/CostComingOutLOL/Figures/3_Mechanisms/Graves_Performance"
 
-produce_plots_pooled(pooled_result_kd_ratio, save_here)
-produce_plots_pooled(pooled_result_win_rate, save_here)
+produce_plots_pooled(pooled_result, save_here)
 
-# LATEX -------------------------------------------------------------------
-n_col <- 3 # Number of outcomes + 1.
+# Point estimate and confidence intervals ---------------------------------
+tau_hat <-  summary(pooled_result$Graves$tau_hat)$estimate
+se <- pooled_result$Graves$se_tau_hat
+cil <- trimws(format(round(tau_hat - 1.96 * se, 3), nsmall = 3))
+ciu <- trimws(format(round(tau_hat + 1.96 * se, 3), nsmall = 3))
 
-tau_hats <- c(pooled_result_kd_ratio$Graves$tau_hat, pooled_result_win_rate$Graves$tau_hat)
-ses <- c(pooled_result_kd_ratio$Graves$se_tau_hat, pooled_result_win_rate$Graves$se_tau_hat)
-cils <- trimws(format(round(tau_hats - 1.96 * ses, 3), nsmall = 3))
-cius <- trimws(format(round(tau_hats + 1.96 * ses, 3), nsmall = 3))
-tau_hats <- format(round(tau_hats, 3), nsmall = 3)
-
-synth_outcomes_dta <- lapply(list(pooled_result_kd_ratio, pooled_result_win_rate), function(z) { lapply(z[!(names(z) %in% c("outcome_colname", "estimator", "donors", "treatment_date", "bandwidth"))], function(x) { construct_synth_outcome(x$tau_hat, x$dta, "champion", "smooth_outcome", "day") }) })
-synth_outcomes <- lapply(synth_outcomes_dta, function(x) { x[["Graves"]]$synth_outcome %>% filter(day < treatment_date) })
-n_donors <- sapply(synth_outcomes_dta, function(x) { x[["Graves"]]$weights %>% nrow()})
-
-dta_kd_ratio <- pooled_result_kd_ratio$Graves$dta %>%
-  filter(champion == "Graves" & day < treatment_date) %>%
-  select(day, smooth_outcome)
-dta_win_rate <- pooled_result_win_rate$Graves$dta %>%
-  filter(champion == "Graves" & day < treatment_date) %>%
-  select(day, smooth_outcome)
-
-rmses_kd_ratio <- format(round(rmse(dta_kd_ratio$smooth_outcome, synth_outcomes[[1]]$synth_outcome), 3), nsmall = 3)
-rmses_win_rate <- format(round(rmse(dta_win_rate$smooth_outcome, synth_outcomes[[2]]$synth_outcome), 3), nsmall = 3)
-
-latex <- function() {
-cat("\\begingroup
-  \\setlength{\\tabcolsep}{8pt}
-  \\renewcommand{\\arraystretch}{1.1}
-  \\begin{table}[b!]
-    \\centering
-    \\begin{adjustbox}{width = 1\\textwidth}
-    \\begin{tabular}{@{\\extracolsep{5pt}}l ", rep("c ", n_col), "}
-      \\\\[-1.8ex]\\hline
-      \\hline \\\\[-1.8ex]
-      & ", str_sub(paste(paste(paste0("(", seq_len(2), ") &"), collapse = " "), collapse = " "), end = -3), " \\\\
-      & ", str_sub(paste(paste(paste0(c("Kills-to-deaths", "Win rate"), " &"), collapse = " "), collapse = " "), end = -3), " \\\\
-      \\addlinespace[2pt]
-      \\hline \\\\[-1.8ex] \n\n", sep = "")
-
-cat("      $\\hat{\\tau}$ & ", str_sub(paste(paste0(tau_hats, " &"), collapse = " "), end = -4), " \\\\
-      $95\\%$ CI & ", str_sub(paste(paste0("[", cils, ", ", cius, "] &"), collapse = " "), end = -3), " \\\\ \n", sep = "")
-
-cat("\n      \\addlinespace[3pt]
-      \\\\[-1.8ex]\\hline
-      \\hline \\\\[-1.8ex]
-      \\end{tabular}
-      \\end{adjustbox}
-      \\caption{Point estimates and $95\\%$ confidence intervals for $\\hat{\\tau}$. Additionally, the number of donors receiving a non-zero weight and the pre-treatment root mean squared error are displayed.}
-      \\label{table_estimation_results_pooled}
-    \\end{table}
-\\endgroup")
-}
-
-latex()
+cat("Point estimate: ", tau_hat, " [", cil, ", ", ciu, "] \n", sep = "")
